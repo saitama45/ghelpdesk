aac3fdff47f8cc62a82121146abb45a961ff08dd
saitama45 <genermagbanua45@gmail.com>
2026-03-15 01:45:06 +0800

    fix(deploy): improve nginx routing and ziggy relative url syntax for azure

--- resources/js/Pages/Settings/Index.vue
+++ resources/js/Pages/Settings/Index.vue
@@ -154,7 +154,7 @@ const testConnection = () => {
     testingConnection.value = true;
     testResult.value = null;
 
-    axios.post(route('settings.test-imap', undefined, false), {
+    axios.post(route('settings.test-imap', {}, false), {
         imap_host: form.imap_host,
         imap_port: form.imap_port,
         imap_encryption: form.imap_encryption,
