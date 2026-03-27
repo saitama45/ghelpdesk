2cf824c7f948a7607d3ac31c6e39eccc6b5a45aa
saitama45 <genermagbanua45@gmail.com>
2026-03-27 22:45:29 +0800

    fix: Correct the config of Redis Cluster in database.php

--- config/database.php
+++ config/database.php
@@ -154,7 +154,6 @@ return [
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
-            'persistent' => env('REDIS_PERSISTENT', false),
             'context' => [
                 'ssl' => [
                     'verify_peer' => false,
@@ -163,34 +162,29 @@ return [
             ],
         ],
 
-        'default' => [
-            'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'username' => env('REDIS_USERNAME'),
-            'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_DB', '0'),
-            'scheme' => env('REDIS_SCHEME', 'tcp'),
-            'ssl' => env('REDIS_SCHEME') === 'tls' ? ['verify_peer' => false, 'verify_peer_name' => false] : null,
-            'max_retries' => env('REDIS_MAX_RETRIES', 3),
-            'backoff_algorithm' => env('REDIS_BACKOFF_ALGORITHM', 'decorrelated_jitter'),
-            'backoff_base' => env('REDIS_BACKOFF_BASE', 100),
-            'backoff_cap' => env('REDIS_BACKOFF_CAP', 1000),
-        ],
-
-        'cache' => [
-            'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'username' => env('REDIS_USERNAME'),
-            'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_CACHE_DB', '1'),
-            'scheme' => env('REDIS_SCHEME', 'tcp'),
-            'ssl' => env('REDIS_SCHEME') === 'tls' ? ['verify_peer' => false, 'verify_peer_name' => false] : null,
-            'max_retries' => env('REDIS_MAX_RETRIES', 3),
-            'backoff_algorithm' => env('REDIS_BACKOFF_ALGORITHM', 'decorrelated_jitter'),
-            'backoff_base' => env('REDIS_BACKOFF_BASE', 100),
-            'backoff_cap' => env('REDIS_BACKOFF_CAP', 1000),
+        'clusters' => [
+            'default' => [
+                [
+                    'host' => env('REDIS_HOST', '127.0.0.1'),
+                    'username' => env('REDIS_USERNAME'),
+                    'password' => env('REDIS_PASSWORD'),
+                    'port' => env('REDIS_PORT', '6379'),
+                    'database' => 0,
+                    'scheme' => env('REDIS_SCHEME', 'tcp'),
+                    'ssl' => ['verify_peer' => false, 'verify_peer_name' => false],
+                ],
+            ],
+            'cache' => [
+                [
+                    'host' => env('REDIS_HOST', '127.0.0.1'),
+                    'username' => env('REDIS_USERNAME'),
+                    'password' => env('REDIS_PASSWORD'),
+                    'port' => env('REDIS_PORT', '6379'),
+                    'database' => 0,
+                    'scheme' => env('REDIS_SCHEME', 'tcp'),
+                    'ssl' => ['verify_peer' => false, 'verify_peer_name' => false],
+                ],
+            ],
         ],
 
     ],
